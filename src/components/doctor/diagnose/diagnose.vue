4a50da628c307751ab711d5217d0c2ff0b6745d3
576510673@qq.com <GSY123456asd>
2020-05-11 19:10:09 +0800

    1

--- src/components/doctor/diagnose/diagnose.vue
+++ src/components/doctor/diagnose/diagnose.vue
@@ -1,13 +1,44 @@
 <template>
+  <div class="layout">
+    <Menu mode="horizontal" theme="dark" active-name="3">
+      <div class="layout-logo"></div>
+      <div class="layout-nav">
+        <Menu-item name="1" @click.native="checkApply">
+          <Icon type="ios-navigate"></Icon>
+          病人病情诊断
+        </Menu-item>
+        <Menu-item name="2" @click.native="personinformation">
+          <Icon type="ios-keypad"></Icon>
+          病人检查方法
+        </Menu-item>
+        <Menu-item name="3" @click.native="during">
+          <Icon type="ios-analytics"></Icon>
+          病人具体用药
+        </Menu-item>
+      </div>
+    </Menu>
+    <div class="layout-content">
 
+    </div>
+  </div>
 </template>
-
 <script>
-    export default {
-        name: "diagnose"
-    }
+
+  export default {
+    data(){
+      return{
+        userId:0,
+      }
+    },
+    created() {
+      this.userId = window.localStorage.getItem("userID")
+    },
+    methods:{
+
+    },
+  }
 </script>
 
 <style scoped>
-
+  @import "diagnose.css";
 </style>
